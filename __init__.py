c26f59915bd918f8e0f0c12a844f16c67e0fa4df
tentan2 <ten.tan17655@gmail.com>
2026-04-18 20:12:42 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,26 @@
 """
-Scripts module for Spotscan
-Contains utility scripts for dataset management and setup
+Spotscan Analysis Module
+Contains specialized analysis modules for different food properties
 """
+
+from .nutrition_analyzer import NutritionAnalyzer
+from .freshness_detector import FreshnessDetector
+from .ripeness_predictor import RipenessPredictor
+from .texture_analyzer import TextureAnalyzer
+from .color_analyzer import ColorAnalyzer
+from .shape_reconstructor import ShapeReconstructor
+from .ocr_analyzer import OCRAnalyzer
+from .liquid_analyzer import LiquidAnalyzer
+from .safety_checker import SafetyChecker
+
+__all__ = [
+    'NutritionAnalyzer', 
+    'FreshnessDetector', 
+    'RipenessPredictor',
+    'TextureAnalyzer', 
+    'ColorAnalyzer', 
+    'ShapeReconstructor',
+    'OCRAnalyzer', 
+    'LiquidAnalyzer', 
+    'SafetyChecker'
+]
